5a16a865f76521b60c00f006fc2499fbcce9e8de
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-08-08 16:54:48 +0530

    Address Book Form

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import AddressBookForm from "./components/address-book-form/address-book-form";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+        <Switch>
+          <Route exact path = "/address-book-form"><AddressBookForm /></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
